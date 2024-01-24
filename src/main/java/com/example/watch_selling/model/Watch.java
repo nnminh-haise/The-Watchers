0b2479da6bb2df067166fd6fd97a22c914ccfecf
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-01-24 07:22:09 +0700

    Created layout for login api

---
+++ src/main/java/com/example/watch_selling/model/Watch.java
@@ -0,0 +1,13 @@
+package com.example.watch_selling.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Watch {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ src/main/java/com/example/watch_selling/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.example.watch_selling.repository;
+
+public class UserRepository {
+
+}
