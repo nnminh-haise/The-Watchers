b44f63043a31733f35024b5a1cce95153d7cc279
nnminh-haise <nnminh.sam.1803@gmail.com>
2024-05-30 01:38:31 +0700

    feat: update email body

--- src/main/java/com/example/watch_selling/service/AuthenticationService.java
+++ src/main/java/com/example/watch_selling/service/AuthenticationService.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Date;
 import java.util.Optional;
 import java.util.Random;
 
